4f228c0ed23a0735726e6eb5696e75f9cc5dbfa4
Joeoo0 <misjabm03@gmail.com>
2023-08-23 02:35:32 +0700

    tambah form

--- perpustakaan/forms.py
+++ perpustakaan/forms.py
@@ -1,1 +1,7 @@
 from django.forms import ModelForm
+from perpustakaan.models import Buku
+
+class FormBuku(ModelForm):
+    class Meta:
+        model = Buku
+        fields = ['judul', 'penulis', 'kelompok_id']
